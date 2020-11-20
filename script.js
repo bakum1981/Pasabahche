9b8b16b132a7be2ec70b1b98aca99727c358c8d1
artem1606 <53149823+artem1606@users.noreply.github.com>
2020-11-21 02:29:57 +0300

    changedAdaptive

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ $(document).ready(function() {
 		$('body').toggleClass('lock');
 	});
 
+	var videoA = function () {this.innerHTML = '<iframe  src="https://www.youtube.com/watch?v=zRCUDjXCfiM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}; 
+	document.querySelector('.item-brend__video').addEventListener('click', function (e) { this.removeEventListener('click', videoA, false); videoA.apply(this, arguments); } , false);	
+
+
 	const cart = this.querySelector('.main__item-cart');
-	const halfHeight = cart.offsetHeight / 2;	
-	const halfWidth = cart.offsetWidth / 2;
-
-	
-
-	const rotate = (event) =>  {		
-		cart.style.transform = 'rotateX('+-(event.offsetY - halfHeight) / 5 + 'deg) rotateY('
-	+ (event.offsetX - halfWidth) / 5 + 'deg)';	
+	const rotate = (event) =>  {
+		
+		const halfHeight = cart.offsetHeight / 2;	
+		const halfWidth = cart.offsetWidth / 2;		
+			cart.style.transform = 'rotateX('+-(event.offsetY - halfHeight) / 5 + 'deg) rotateY('
+		+ (event.offsetX - halfWidth) / 5 + 'deg)';	
 	}
 
+	if(cart){
+		cart.addEventListener('mousemove', rotate);
+	}
 
-	cart.addEventListener('mousemove', rotate);
-
+	$('.close').click(function(){
+		$('.popap').addClass('popap-hide');
+	});
+	
 
 });
